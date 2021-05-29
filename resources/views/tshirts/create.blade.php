b55dafad80d9ed8865555658d783557d372de960
Jerry Almeida <jqueryalmeida@gmail.com>
2021-05-29 20:56:20 +0100

    Update create.blade.php
    fix: Layout composition

--- resources/views/tshirts/create.blade.php
+++ resources/views/tshirts/create.blade.php
@@ -3,51 +3,34 @@
 @section('content')
 
     <div class="container">
+        <!-- Product section-->
+        <section class="py-5">
+            <div class="row gx-4 gx-lg-5 align-items-center">
 
-
-            <!-- Product section-->
-            <section class="py-5">
-                <div class="row gx-4 gx-lg-5 align-items-center">
-            
-                    <div class="col-md-6">
-                            <div class="layered-image p-2 bd-highlight">
-                                <img class="image-base img-thumbnail" src="{{asset('storage/tshirt_base/' . $corSel->codigo . '.jpg')}}"
-                                    alt="tshirt base"/>
-                                <img class="image-overlay" src="{{asset('storage/estampas/' . $estampa->imagem_url)}}" alt="{{$estampa->nome}}"/>
-                            </div>
-
-            @csrf
-            <!-- route('tshirts.chooseWithColor', ['estampa' => $estampa, 'cor' => $corSel])}} -->
-
-            <div class="filter p-2 flex-grow-1">
-                <form class="cor-search" action="#" method="GET">
-                    <div class="search-item">
-                        <label for="idCor">Escolha a cor: </label>
-                        <select class="form-control" name="cor" id="idCor">
-                            @foreach ($listaCores as $cor)
-                                <option
-                                    value="{{$cor->codigo}}" {{$corSel->codigo == $cor->codigo ? 'selected' : ''}}>
-                                    {{$cor->nome}}
-                                </option>
-                            @endforeach
-                        </select>
+                <div class="col-md-6">
+                    <div class="layered-image p-2 bd-highlight">
+                        <img class="image-base img-thumbnail magic-shirt"
+                             src="{{asset('storage/tshirt_base/' . $corSel->codigo . '.jpg')}}"
+                             alt="tshirt base"/>
+                        <img class="image-overlay" src="{{asset('storage/estampas/' . $estampa->imagem_url)}}"
+                             alt="{{$estampa->nome}}"/>
                     </div>
-                    <div class="col-md-6">
-                        <h1 class="display-5 font-weight-bold">{{$estampa->nome}}</h1>
-                        <div class="fs-5 mb-3">
+                </div>
+
+                <div class="col-md-6">
+                    <h1 class="display-5 font-weight-bold">{{$estampa->nome}}</h1>
+                    <div class="fs-5 mb-3">
                         @if($estampa->client_id)
                             <h4>{{$preco->preco_un_proprio}}</h4>
                         @else
                             <h4>{{$preco->preco_un_catalogo}}</h4>
                         @endif
-                        </div>
-                        <p class="lea">{{$estampa->descricao}}</p>
-                        <form action="" method="GET">
-                        <!--@csrf-->
-                        <div class="form-row">
+                    </div>
+                    <p class="lea">{{$estampa->descricao}}</p>
+                    <div class="form-row mb-3">
                         <div class="col-md-5 my-1">
-                            <label for="idCor">Escolha a cor: </label>
-                            <select class="form-control" name="cor" id="idCor">
+                            <label for="idCor" class="text-muted">Escolha a cor: </label>
+                            <select class="form-control magic-color" name="cor" id="idCor">
                                 @foreach ($listaCores as $cor)
                                     <option
                                         value="{{$cor->codigo}}" {{$corSel->codigo == $cor->codigo ? 'selected' : ''}}>
@@ -56,44 +39,51 @@
                                 @endforeach
                             </select>
                         </div>
-          
-                        <div class="col-md-4 my-1">
-                            <label for="selectTamanho">Escolha o tamanho: </label>
-                            <select class="form-control" name="tamanho" id="selectTamanho">
-                                <option>XS</option>
-                                <option>S</option>
-                                <option>M</option>
-                                <option>L</option>
-                                <option>XL</option>
-                            </select>
-                            </div>
-                        </div><!--/form-row-->
-                        <div class="form-row mt-3">
+                    </div>{{--/form-row--}}
+                    <div class="form-group mb-3">
+                        <label class="text-muted">Tamanhos</label>
+                        <div class="btn-groupx btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-size">
+                                <input type="radio" name="size" value="xs" autocomplete="off"> XS
+                            </label>
+                            <label class="btn btn-size active">
+                                <input type="radio" name="size" value="s" autocomplete="off" checked> S
+                            </label>
+                            <label class="btn btn-size">
+                                <input type="radio" name="size" value="m" autocomplete="off"> M
+                            </label>
+                            <label class="btn btn-size">
+                                <input type="radio" name="size" value="l" autocomplete="off"> L
+                            </label>
+                            <label class="btn btn-size">
+                                <input type="radio" name="size" value="xl" autocomplete="off"> XL
+                            </label>
+                        </div>
+                    </div>{{--/form-group--}}
+
+                    <div class="form-row mb-3">
                         <div class="col-md-3 my-1">
-                            <select class="form-control" name="qty">
-                            @foreach (range(1, 10) as $qty)
-                                <option>{{$qty}}</option>
-                            @endforeach
-                            </select>
-                            </div>
-                            <div class="col-md-5 my-1">
-                                <button class="btn btn-outline-dark" type="submit">
-                                    Selecionar
+                            <div class="number-input  mb-0">
+                                <button type="button"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
+                                        class="minus py-2 font-weight-bold">-
+                                </button>
+                                <input class="quantity" min="0" max="10" name="quantity" value="1" type="number">
+                                <button type="button"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
+                                        class="plus py-2 font-weight-bold">+
                                 </button>
                             </div>
-                        </div><!--/form-row-->
-                        <hr>
-                        <div class="form-row mt-3">
-                                <button class="btn btn-lg btn-primary flex-shrink-0" type="button">
-                                    <i class="fas fa-shopping-bag"></i>
-                                    Adicionar ao carrinho
-                                </button>
-                        </div><!--/form-row-->
-                        </form>
-                    </div>
-                </div>
+                        </div>
+                        <div class="col-auto my-1">
+                            <button class="btn btn-add-cart btn-primary flex-shrink-0" name="add-to-cart" type="submit">
+                                <i class="fas fa-shopping-bag mr-2"></i>
+                                Adicionar ao carrinho
+                            </button>
+                        </div>
+                    </div>{{--/form-row--}}
+                </div>{{--/col-md-6--}}
+            </div>
         </section>
-    
-    </div>
     </div>
 @endsection
