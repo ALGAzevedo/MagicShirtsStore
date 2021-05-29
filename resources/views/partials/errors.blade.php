98d25dd0fa75592f9f9b976d991c5737b3a18e11
Jerry Almeida <jqueryalmeida@gmail.com>
2021-05-29 23:04:04 +0100

    Create errors.blade.php

---
+++ resources/views/partials/errors.blade.php
@@ -0,0 +1,7 @@
+<div class="alert alert-danger fade show" role="alert">
+    <ul>
+        @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </ul>
+  </div>
