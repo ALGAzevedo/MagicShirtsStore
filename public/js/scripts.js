da3e49853e1856024ba387d6cac9b33db71578b3
Jerry Almeida <jqueryalmeida@gmail.com>
2021-05-31 00:48:20 +0100

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,16 @@
 
+
+$(function() {
+  
+    $(".quantity").on('change paste input', function() {
+        console.log($(this).val())
+    });
+
+   /* $("select").on('change', function() {
+        $(this).attr("src", "url");
+    });*/
+});
+
 const magic_color = document.querySelector('.magic-color')
 const magic_shirt = document.querySelector('.magic-shirt')
 const STORAGE_URL = magic_shirt.getAttribute('data-storage')
