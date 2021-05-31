645d47328bf272bcc776591c4c8335e5cc65c707
Jerry Almeida <jqueryalmeida@gmail.com>
2021-05-31 23:21:56 +0100

    Added pageTitle

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,6 +12,7 @@ class PageController extends Controller
     }
 
     public function about(){
-        return view('pages.about');
+        return view('pages.about')
+            ->with('pageTitle', 'About');
     }
 }
