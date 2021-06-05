47a098b74f4f3e449c75595495966786b7a38842
André Azevedo <andrelgdeazevedo@gmail.com>
2021-06-05 13:46:40 +0100

    Created Request class for cliente update password

---
+++ app/Http/Requests/ClientePasswordUpdatePost.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientePasswordUpdatePost extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+
+    public function rules()
+    {
+        return [
+            'oldPassword' => 'required',
+            'newPassword' => ['required', 'string', 'min:8', 'confirmed'],
+            'newPassword_confirmation' => ['required', 'string', 'min:8'],
+        ];
+    }
+}
