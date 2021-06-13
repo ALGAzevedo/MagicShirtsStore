e3bb165e27a989514c20d47c6345a20d2ed2f5a5
JoaoFerreiraIPL <2191844@my.ipleiria.pt>
2021-06-13 21:45:30 +0100

    update on statistics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
+use App\Models\Encomenda;
+use App\Models\Estampa;
+use App\Models\Tshirt;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+use mysql_xdevapi\Table;
 
 class DashboardController extends Controller
 {
+
     public function index(){
+
+        //vendas este mes €
+        $vendasMes = Encomenda::whereYear('data', Carbon::now()->year)
+        ->whereMonth('data', Carbon::now()->month)
+            ->sum('preco_total');
+
+        //encomendas a aguardar processamento (em estado pago)
+        $encomendasPagas = Encomenda::where('estado', 'paga')->count();
+
+        //intens vendidos hoje
+        $vendasHoje = Encomenda::where('data', Carbon::now()->format('Y-m-d'))->sum('preco_total');
+
+        //registo clientes hoje
+        $novosClientesHoje = Cliente::whereDate('created_at', Carbon::today())->count();
+
+        //ESTAMPAS DISPONIVEIS NA LOJA
+        $estampasDisponiveis = Estampa::where('cliente_id', null)->withoutTrashed()->count();
+
+        //T-SHIRTS CUSTOMIZADAS VENDIDAS
+        $shirtsClient = DB::Table('estampas')
+            ->leftJoin('tshirts', 'estampas.id', '=', 'tshirts.estampa_id')
+            ->Join('encomendas', 'tshirts.encomenda_id', '=','encomendas.id')
+            ->whereNotNull('estampas.cliente_id')
+            ->where('encomendas.estado', 'fechada')
+            ->count();
+
+
+
+
+
 
         $chart_options = [
             'chart_title' => 'Transações por dia na última semana',
@@ -79,6 +117,7 @@ class DashboardController extends Controller
 
 
 
-        return view('administracao.index',compact('chart1', 'chart2', 'chart3', 'chart4'));
+        return view('administracao.index',compact('chart1', 'chart2', 'chart3', 'chart4', 'vendasMes',
+        'encomendasPagas', 'vendasHoje', 'novosClientesHoje', 'estampasDisponiveis', 'shirtsClient'));
     }
 }
--- resources/views/administracao/index.blade.php
+++ resources/views/administracao/index.blade.php
@@ -2,39 +2,25 @@
 @can('viewCharts')
 @section('content')
     <div class="no-gutters row">
-        <div class="cl col-lg-3">
+        <div class="cl col-lg-2">
             <div class="card br-0 card-body mb-2 mb-md-0">
                 <div class="card-resume">
                     <span class="icon icon-sm rounded-circle bg-primary-light"><i class="text-primary fas fa-euro-sign"></i></span>
                     <div class="text">
 
-                        <span>13,456 €</span>
+                        <span>{{$vendasMes}}€</span>
                         <h6 class="mb-0 card-title">vendas</h6>
                         <span class="text-sm">líquidas este mês </span>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="cl col-lg-3">
-            <div class="card br-0 card-body  mb-2 mb-md-0">
-                <div class="card-resume">
-                    <span class="icon icon-sm rounded-circle bg-success-light"><i class="text-success fas fa-box-open"></i></span>
-                    <div class="text">
-                        <span>3</span>
-                        <h6 class="mb-0 card-title">encomendas</h6>
-                        <span class="text-sm">
-                                    a aguardar processamento
-                                </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="cl col-lg-3">
+        <div class="cl col-lg-2">
             <div class="card br-0 card-body  mb-2 mb-md-0">
                 <div class="card-resume">
                     <span class="icon icon-sm rounded-circle bg-warning-light"><i class="text-warning fas fa-shopping-basket"></i></span>
                     <div class="text">
-                        <span>9.856€</span>
+                        <span>{{$vendasHoje}}€</span>
                         <h6 class="mb-0 card-title">Itens vendidos</h6>
                         <span class="text-sm">
                                     Total de vendas hoje
@@ -43,12 +29,54 @@
                 </div>
             </div>
         </div>
-        <div class="cl col-lg-3">
+        <div class="cl col-lg-2">
+            <div class="card br-0 card-body  mb-2 mb-md-0">
+                <div class="card-resume">
+                    <span class="icon icon-sm rounded-circle bg-success-light"><i class="text-success fas fa-box-open"></i></span>
+                    <div class="text">
+                        <span>{{$encomendasPagas}}</span>
+                        <h6 class="mb-0 card-title">encomendas</h6>
+                        <span class="text-sm">
+                                    a aguardar processamento
+                                </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cl col-lg-2">
+            <div class="card br-0 card-body  mb-2 mb-md-0">
+                <div class="card-resume">
+                    <span class="icon icon-sm rounded-circle bg-light"><i class="text-secundary fas fa-tshirt"></i></span>
+                    <div class="text">
+                        <span>{{$estampasDisponiveis}}</span>
+                        <h6 class="mb-0 card-title">estampas</h6>
+                        <span class="text-sm">
+                                    disponiveis da loja
+                                </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cl col-lg-2">
+            <div class="card br-0 card-body  mb-2 mb-md-0">
+                <div class="card-resume">
+                    <span class="icon icon-sm rounded-circle bg-warning-light"><i class="text-danger fas fa-paint-roller"></i></span>
+                    <div class="text">
+                        <span>{{$shirtsClient}}</span>
+                        <h6 class="mb-0 card-title">t-shirts</h6>
+                        <span class="text-sm">
+                                    estampa cliente vendidas
+                                </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cl col-lg-2">
             <div class="card br-0 card-body  mb-2 mb-md-0">
                 <article class="card-resume">
                     <span class="icon icon-sm rounded-circle bg-info-light"><i class="text-info fas fa-user-alt"></i></span>
                     <div class="text">
-                        <span>0</span>
+                        <span>{{$novosClientesHoje}}</span>
                         <h6 class="mb-0 card-title">registo</h6>
                         <span class="text-sm">
                                     de novos clientes
